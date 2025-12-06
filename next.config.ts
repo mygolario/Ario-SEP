bd71587b4d8fd8aa512e48550c8e6ddcab62631b
mygolario <ariokaveh85@gmail.com>
2025-12-06 23:51:37 +0330

    fix: replace next.config csp with robust middleware csp

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com; object-src 'none';",
-          },
-        ],
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
