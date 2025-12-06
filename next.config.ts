028f637f6d34393685ba8a872402c794a26ac585
mygolario <ariokaveh85@gmail.com>
2025-12-06 22:33:40 +0330

    fix: allow unsafe-eval in CSP to resolve runtime errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com; object-src 'none';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
