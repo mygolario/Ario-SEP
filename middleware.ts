0acf987109def4c29c65c61ab6af4b6da00f13d6
mygolario <ariokaveh85@gmail.com>
2025-12-07 12:54:25 +0330

    fix(csp): allow google avatar images

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export function middleware(request: NextRequest) {
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com;
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
+    img-src 'self' blob: data: https://*.googleusercontent.com https://lh3.googleusercontent.com;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
